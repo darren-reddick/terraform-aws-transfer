064991981aab5fb110580b658aadaa7522f0b727
Darren Reddick <57802771+darren-reddick@users.noreply.github.com>
2021-10-06 21:23:46 +0100

    OpenAPI spec fix (#16)
    * openapi fix

--- apigateway.tf
+++ apigateway.tf
@@ -1,31 +1,31 @@
 resource "aws_iam_role" "iam_for_apigateway_idp" {
   name = "iam_for_apigateway_idp"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
+  assume_role_policy = <<-EOF
     {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "apigateway.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "apigateway.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
     }
-  ]
-}
-EOF
+  EOF
 }
 
 
 resource "aws_iam_role_policy_attachment" "apigateway-cloudwatchlogs" {
-  role       = "${aws_iam_role.iam_for_apigateway_idp.name}"
+  role       = aws_iam_role.iam_for_apigateway_idp.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
 }
 
 resource "aws_api_gateway_account" "api_gateway_account" {
-  cloudwatch_role_arn = "${aws_iam_role.iam_for_apigateway_idp.arn}"
+  cloudwatch_role_arn = aws_iam_role.iam_for_apigateway_idp.arn
 }
 
 resource "aws_api_gateway_rest_api" "sftp-idp-secrets" {
@@ -40,16 +40,17 @@ resource "aws_api_gateway_rest_api" "sftp-idp-secrets" {
 resource "aws_lambda_permission" "allow_apigateway" {
   statement_id  = "AllowExecutionFromApigateway"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.sftp-idp.function_name}"
+  function_name = aws_lambda_function.sftp-idp.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.sftp-idp-secrets.execution_arn}/*/*/*"
 }
 
 resource "aws_api_gateway_deployment" "prod" {
-  rest_api_id = "${aws_api_gateway_rest_api.sftp-idp-secrets.id}"
+  rest_api_id = aws_api_gateway_rest_api.sftp-idp-secrets.id
   triggers = {
     redeployment = sha1(jsonencode([aws_api_gateway_rest_api.sftp-idp-secrets.body]))
   }
+
   lifecycle {
     create_before_destroy = true
   }
@@ -57,7 +58,7 @@ resource "aws_api_gateway_deployment" "prod" {
 
 resource "aws_api_gateway_stage" "prod" {
   stage_name    = "prod"
-  rest_api_id   = "${aws_api_gateway_rest_api.sftp-idp-secrets.id}"
-  deployment_id = "${aws_api_gateway_deployment.prod.id}"
+  rest_api_id   = aws_api_gateway_rest_api.sftp-idp-secrets.id
+  deployment_id = aws_api_gateway_deployment.prod.id
 }
 
--- examples/public-dynamo/output.tf
+++ examples/public-dynamo/output.tf
@@ -1,8 +1,11 @@
+output "dynamo_table_name" {
+  value = module.idp.dynamo_table_name
+}
+
 output "endpoint" {
-    value = "${aws_transfer_server.sftp.endpoint}"
+  value = aws_transfer_server.sftp.endpoint
 }
 
 output "role" {
-    value = "${aws_iam_role.foo.arn}"
+  value = aws_iam_role.foo.arn
 }
-
